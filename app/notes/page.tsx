20b09c9276b62733362bdf31b5474102d6ea6ed6
AbhishekSingh <siyanisabhishek@gmail.com>
2024-01-20 15:31:42 +0530

    offloaded webhooks by clerk

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,13 +1,18 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React from "react";
 import { auth } from "@clerk/nextjs";
 import Note from "@/lib/database/models/note.model";
+import connectToDatabase from "@/lib/database";
 
 const NotesPage = async () => {
   const { userId } = auth();
+  if (!userId) throw new Error("userId is undefined");
+
+  await connectToDatabase();
+  const allNotes = await Note.find({ clerkId: userId });
+
   if (!userId) throw new Error("Userid is undefined");
 
-  return <div>NotesPage</div>;
+  return <div>{JSON.stringify(allNotes)}</div>;
 };
 
 export default NotesPage;
